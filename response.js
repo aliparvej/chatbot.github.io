2c864acf30738f011117f2d0b8c6fc8d99b2255c
parvejali1234 <105584606+parvejali1234@users.noreply.github.com>
2022-11-21 14:21:34 +0530

    Add files via upload

---
+++ response.js
@@ -0,0 +1,9 @@
+const responseObj = {
+    hello:"hey! how are you doing ?",
+    Hi:"hello",
+    hi:"hello",
+    hey: "hey! what's  Up",
+    today: new Date().toDateString(),
+    time: new Date().toLocaleTimeString(), 
+    
+};
